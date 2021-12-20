4847a3fde52fae7b08a91290abd218f053f1bab8
chnorami <chnorami@espol.edu.ec>
2021-12-19 22:22:55 -0500

    ventana perfilusuario

--- app/src/main/java/com/example/pst_ta5_g5/PerfilUsuario.java
+++ app/src/main/java/com/example/pst_ta5_g5/PerfilUsuario.java
@@ -2,13 +2,23 @@ package com.example.pst_ta5_g5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilUsuario extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
+
     }
+
+    public void salir(View v){
+        Intent i= new Intent(getApplicationContext(),MenuOpciones.class);
+        startActivity(i);    }
+
 }
